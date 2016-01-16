76b330408e3f6a0fb92712fbb72e49c50c4f1962
MJafar Mashhadi <raindigital2007@gmail.com>
2016-01-16 03:39:25 +0330

    elastic, search api

--- elastic/search_api.py
+++ elastic/search_api.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class SearchAPI:
 
     def __init__(self, base_url):
@@ -6,4 +9,47 @@ class SearchAPI:
         :return:
         """
         self.base_url = base_url
+        if self.base_url[-1] == '/':
+            self.base_url = self.base_url[:-1]
 
+    def get_search_url(self, index=None, doc_type=None):
+        """
+        Get url for sending search request. If index is
+        :param index: name of one index or list of index names
+        :param doc_type: name of one doc type or list of doctypes
+        :return:
+        """
+        url_parts = [self.base_url]
+        if index:
+            if type(index) == list:
+                index = ','.join(index)
+            url_parts.append(index)
+
+            if doc_type:
+                if type(doc_type) == list:
+                    doc_type = ','.join(doc_type)
+                url_parts.append(doc_type)
+        elif doc_type:
+            raise Exception('Cannot search in a given type without knowing index name. yet.')
+
+        url_parts.append('_search')
+
+        return '/'.join(url_parts)
+
+    def search(self, query, index=None, type=None, size=1000):
+        """
+        :param query: A dictionary of query in fields
+        :param index: name of one index or list of index names
+        :param type: name of one doc type or list of doctypes
+        :param size: number of top docs returned. size of hits array in response
+        :return: { "total": 15, "hits": [{...}]}
+        """
+        base_url = self.get_search_url(index, type)
+        query_string_items = []
+        for k,v in query.key():
+            query_string_items.append('{}:{}'.format(k,v))
+
+        query_string = ','.join(query_string_items)
+        query_url = '{}?q={}&sort=pagerank:desc&size={}'.format(base_url, query_string, size)
+
+        return requests.get(query_url).json()['hits']
